8c1c21995e1508da187a8c30870493b815db1c8b
Tanna <tanna1987@gmail.com>
2024-02-26 18:10:31 +0800

    Complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Page from "./components/Page"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Page />
+        </div>
+    )
+}
